42d3e21863126e6a720987b0fd1881d30cdc1fcf
alien202304 <131525515+alien202304@users.noreply.github.com>
2024-10-01 19:15:35 +0300

    Update main.tf

--- Hometask_1/main.tf
+++ Hometask_1/main.tf
@@ -1,7 +1,66 @@
-variable "first_var" {
-
-    type="string"
-    
-    
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
 }
 
+provider "yandex" {
+  token     = "y0_AgAAAABeX4xwAATuwQAAAAESwEvyAABhpqdfEolF5Y4jigWdcHOt3dKF2w"
+  zone      = "ru-central1-a"
+  cloud_id  = "b1gv6fobvoahl9ef8g5r"
+  folder_id = "b1ghca5v18h1eoenfmkk"
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "10"
+  image_id = "fd801rku4j14mv7fs703"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    #ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    ssh-keys  = "/Users/smikheev/.ssh/id_rsa.pub"
+    user-data = "${file("meta.txt")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
