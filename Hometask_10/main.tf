8d0747eed9295d07bcb5234b6a3429b0b0ce646c
alien202304 <131525515+alien202304@users.noreply.github.com>
2024-10-14 19:51:44 +0300

    Update main.tf

--- Hometask_10/main.tf
+++ Hometask_10/main.tf
@@ -8,22 +8,15 @@ terraform {
 }
 
 provider "proxmox" {
-  # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
   pm_api_url = var.api_proxmox_address
-  
-  # api token id is in the form of: <username>@pam!<tokenId>
   pm_api_token_id = var.api_token_id
-
-  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
   pm_api_token_secret = var.api_token_secret
-
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
   pm_parallel = 1
 }
 
 resource "proxmox_vm_qemu" "test_server" {
-  count = 2 
+  count = 2 # Создаем 2 VM
   name = "debian12-vm-${count.index + 1}" 
   target_node = var.proxmox_host
   clone = var.template_name
