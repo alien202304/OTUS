30eaeaf0de9a7b59bb812eb6bae8554831e04190
alien202304 <131525515+alien202304@users.noreply.github.com>
2024-10-14 19:56:29 +0300

    Update vars.tf

--- Hometask_10/vars.tf
+++ Hometask_10/vars.tf
@@ -2,14 +2,17 @@ variable "ssh_key" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCnLXoDfmcqTHY8CUd84lMHLyeAaGqfsqffK99A8aWWJhGGCZDblNMmpIxrroftxAo/MzLphCFGFq2PDZaIwOu+dAuw5qAn/WptrUjkW/lIfGerJeJyJBBSysLSTf5g69Zz2Wnp8lXIY9RJf0aHKujQ07lSdf4UtviTFIdx7bvDJm8qw/EkzjaCPYHmM4+7i8A0s4HLI17VXREq2FC4u9A2Y9MeIt/2NfPn46jw0H2Lv6uaj3HKmEsJe9srYtL+KhncFXsftcv9o0Xb2vyBhRYixbEBS3xVR1bwOdKKr4CBtsJoztQuO0Gp9H4tL8uOO+HfrhoXQp0Za4luJBVC6MDPGivDTNuTK7x0gaf+vqaPc633+6OPQMMjh5b1S65rWiEhQVrP+wd7eiblMWZHk6YJiGSDW84xAcTuAVKQn7GWxZFcyeE/HzTReX9fO/P84u5EaOy57LloL5Zs/R8uQr93khW4eT2TTz72F7w6qtNDXl4hOWbbrd5O7GxWofwZvuHwLzveozodPgwpWGWJq7NWTV2DJ6fyCmba/MkVacV8XEl7+p8KG6DLqJQknjysUW3tCl3mQLn4v0z1CtCi6fcamPvzoC1dilkzR/2bld4lr5s3fzZBoJ/BZoQyvYVbV/FrhJqZAU/o5T0MT3IWCIYmzg2jN5kkqZ4x9vWPheQETw== smikheev@MacBook-Air-Sergei.local"
 }
 
+# Определяем хост ProxMox кластера на котором будут созданы VM
 variable "proxmox_host" {
     default = "pve-01"
 }
 
+# Определяем какой темплейт будет использован для разворачивания VM
 variable "template_name" {
     default = "template-debian-12-initcloud2"
 }
 
+# Опеределяем место хранения дисков VM
 variable "storage_name" {
     default = "ceph_pool_1"
 }
