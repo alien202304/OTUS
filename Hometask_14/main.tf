138a36117afb8a2f6b1ead13025e48dfeda3a5e1
alien202304 <131525515+alien202304@users.noreply.github.com>
2024-10-23 18:11:51 +0300

    Update main.tf

--- Hometask_14/main.tf
+++ Hometask_14/main.tf
@@ -52,23 +52,22 @@ resource "virtualbox" "ceph_nodes" {
     }
   }
   # Настраиваем использование в VirtualBox сетевого моста vmbr0, который подключен к mgmt сети кластера
-  network {
-    model = "virtio"
-    bridge = "vboxnet1"
-    firewall = false
-    link_down = false 
+  network_adapter {
+    type = "bridge"
+    host_interface = "vboxnet1"
   }
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
+  network_adapter {
+    type = "hostonly"
+    host_interface = "vboxnet2"
   }
-  
+
+
   # Опеределяем порядок загрузки
   boot = "order=scsi0"
 
   # Настраиваем IP адреса VM из пула адресов mgmt подсети 192.168.101.131, 192.168.101.132 и 192.168.101.133
   ipconfig0 = "ip=192.168.101.13${count.index + 1}/24,gw=192.168.101.1"
+  ipconfig1 = "ip=10.10.201.13${count.index + 1}/24,gw=10.10.201.1"
   
   # Настраиваем креды пользователя и ssh ключ
   ciuser="root"
